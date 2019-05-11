08aab1c95c6166599b08e824fc096fb6e7f897e6
Ravi Pal <ravikantpal123@gmail.com>
2019-05-11 22:36:05 +0530

    firstCommit

---
+++ DatingApp.API/Migrations/20190511160154_SeedDatabase.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class SeedDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
